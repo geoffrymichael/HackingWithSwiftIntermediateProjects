bd26115cdee5ca53f6d21201411dd84651b44bf3
Geoffry Gambling <groffulet@gmail.com>
2021-05-10 19:59:54 -0700

    Initial setup and basic UI. Import CoreLocation and create a corelocation manager object and make our viewconrtoller its delegate

---
+++ Project22/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Project22
+//
+//  Created by Geoffry Gambling on 5/10/21.
+//
+
+import CoreLocation
+import UIKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet var distanceReading: UILabel!
+    var locationManager: CLLocationManager?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        
+        view.backgroundColor = .gray
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    
+                }
+            }
+        }
+    }
+}
+
